1fb956a80185f4dc6ed41bc1ebe2dbc18e9ce3a9
supergcl <983944994@qq.com>
2017-05-06 17:30:53 +0800

    first commit

---
+++ SaoLei/Mine.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaoLei
+{
+    public class Mine
+    {
+        private int width { set; get; }
+        private int height { set; get; }
+        private int mineNumber { set; get; }
+        private int[,] map { set; get; }
+        private int[] direction;//用来遍历中心周围八个位置
+        /// <summary>
+        /// 游戏状态，0-未开始，1-进行中，2-已结束
+        /// </summary>
+        public int status { private set; get; }
+
+        public void Init(int width=12, int height = 12, int number = 9)
+        {
+            this.width = width;
+            this.height = height;
+            this.mineNumber = number;
+            this.status = 0;
+            direction = new int[3]{ 0,1,-1};
+
+            map = new int[height, width];
+        }
+
+        public void start()
+        {
+            //初始化地图
+            for(int i=0;i<height;i++)
+            {
+                for(int j=0;j<width;j++)
+                {
+                    map[i,j] = -1;
+                }
+            }
+
+            //初始化地雷
+            Random rand = new Random();
+            for(int i=0;i<mineNumber;)
+            {
+                int row = rand.Next(height - 2) + 1;
+                int col = rand.Next(width - 2) + 1;
+                if(map[row, col] != -2)
+                {
+                    map[row, col] = -2;//-2代表地雷
+                    i++;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 输出地图
+        /// </summary>
+        public void show(bool showAll=false)
+        {
+            //输出头部信息
+            Console.Write("   |");
+            for(int i=1;i<width-1;i++)
+            {
+                Console.Write("{0,2} ", i);
+            }
+            Console.WriteLine("\n---|-------------------------------->col");
+
+            //输出地图信息
+            if(showAll)
+            {
+                for (int row = 1; row < height - 1; row++)
+                {
+                    Console.Write("{0,2} |", row);
+                    for (int col = 1; col < width - 1; col++)
+                    {
+                        if (map[row, col] == -1)
+                        {
+                            Console.Write("{0,2} ", "#");
+                        }
+                        else if (map[row, col] == -2)
+                        {
+                            Console.Write("{0,2} ", "*");
+                        }
+                        else
+                        {
+                            Console.Write("{0,2} ", map[row, col]);
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                for (int row = 1; row < height - 1; row++)
+                {
+                    Console.Write("{0,2} |", row);
+                    for (int col = 1; col < width - 1; col++)
+                    {
+                        if(map[row, col] == -1 || map[row, col] == -2)
+                        {
+                            Console.Write("{0,2} ", "#");
+                        }
+                        else
+                        {
+                            Console.Write("{0,2} ", map[row, col]);
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            //输出底部信息
+            Console.WriteLine("   |");
+            Console.WriteLine("   V");
+            Console.WriteLine("  row");
+        }
+
+        /// <summary>
+        /// 计算当前位置周围8个位置中地雷的总数
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="col"></param>
+        /// <returns></returns>
+        public int calculate(int row, int col)
+        {
+            int counter = 0;
+            for(int i=0;i<3;i++)
+            {
+                for(int j=0;j<3;j++)
+                {
+                    if(map[row+direction[i],col+direction[j]] == -2)
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            return counter;
+        }
+
+        /// <summary>
+        /// 选中[row,col]位置
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="col"></param>
+        public void click(int row, int col)
+        {
+            //输入的位置不合法
+            if (row <= 0 || row > (height - 2) || col <= 0 || col > (width - 2)) return;
+
+            //如果当前位置是-2（雷），则游戏结束
+            if(map[row, col] == -2)
+            {
+                status = 2;//游戏结束
+                Console.WriteLine("You lose!");
+                return;
+            }
+
+            int number = calculate(row, col);
+            //如果当前位置周围的地雷数量为0，则显示周围8个格子的数值，并自动递归拓展周围值为0的格子
+            if (0 == number)
+            {
+                //首先设置当前位置为0
+                map[row, col] = 0;
+
+                for(int i = 0;i<3;i++)
+                {
+                    for(int j=0;j<3;j++)
+                    {
+                        if(row+direction[i] <= (height-2) && col+direction[j] <= (width-2) && row+direction[i] >=1 && col+direction[j] >= 1 && //定义边界
+                           !(direction[i] == 0 && direction[j] == 0) && //两个方向不能同时为0，否则会递归调用自身
+                           map[row+direction[i], col+direction[j]] == -1 //保证不能返回调用
+                            )
+                        {
+                            click(row + direction[i], col + direction[j]);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                map[row, col] = number;
+            }
+
+            //检查游戏是否结束
+            if(isOver() == true)
+            {
+                Console.WriteLine("You Win!");
+            }
+        }
+
+        /// <summary>
+        /// 检查游戏是否结束
+        /// </summary>
+        /// <returns></returns>
+        public bool isOver()
+        {
+            int count = 0;
+
+            for(int row =1;row < height -1; row++)
+            {
+                for(int col = 1; col < width - 1; col++)
+                {
+                    if(map[row,col] == -1)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count == 0;
+        }
+    }
+}
---
+++ SaoLei/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace SaoLei
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mine game = new Mine();
+            game.Init();
+            game.start();
+
+            int row, col;
+            string line;
+
+            while(true)
+            {
+                game.show();
+
+                //show some message
+                Console.WriteLine("Please Input the location of row and col:[row, col]:");
+
+                line = Console.ReadLine();
+                string[] numbers = line.Split(new char[] {' '});
+                if (numbers.Length != 2) continue;
+
+                row = Convert.ToInt32(numbers[0]);
+                col = Convert.ToInt32(numbers[1]);
+
+                game.click(row, col);
+
+                //判断游戏是否结束，结束了则退出循环
+                if(game.status == 2)
+                {
+                    break;
+                }
+            }
+
+            //游戏结束后，输出所有地雷的位置
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine("-2 means mime!");
+            game.show(true);
+        }
+    }
+}
